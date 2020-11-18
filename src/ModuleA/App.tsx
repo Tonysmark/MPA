7195c2e8219760e25111bc04e40b3d48855a10c6
tony <mjyuyu@qq.com>
2020-11-18 16:02:42 +0800

    小修改

--- src/ModuleA/App.tsx
+++ src/ModuleA/App.tsx
@@ -3,22 +3,35 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import { Routes } from './router';
 import '../theme/index.scss';
-ReactDOM.render(
-    <StrictMode>
+
+// TODO 如果牵扯到 复杂路由嵌套怎么办？？？
+
+const Root = () => {
+    return (
         <BrowserRouter basename='ModuleA'>
             {Routes.map(({ path }) => (
                 <>
-                    <Link key={path} to={path}>{path}</Link>
+                    <Link key={path} to={path}>
+                        {path}
+                    </Link>
                     <br />
                 </>
             ))}
             <Switch>
                 <Route path='/' exact component={Routes[0].component} />
-                {Routes.map(({ path, component }) => (
-                    <Route key={path} path={path} component={component} />
+                {Routes.map(({ path, loadChildren }) => (
+                    <Route key={path} path={path} component={loadChildren} />
                 ))}
             </Switch>
         </BrowserRouter>
+    );
+};
+
+
+
+ReactDOM.render(
+    <StrictMode>
+        <Root />
     </StrictMode>,
     document.querySelector('#app'),
 );
--- src/ModuleA/router.ts
+++ src/ModuleA/router.ts
@@ -1,18 +1,22 @@
 import loadable, { LoadableComponent } from '@loadable/component';
-import {  ComponentType } from 'react';
-interface ModuleRoute{
+import { ComponentType } from 'react';
+
+// NOTE 坐等 React router V6
+
+interface ModuleRoute {
     path: string;
-    component?: ComponentType;
-    loadChildren?: LoadableComponent<ComponentType>;
+    component?: ComponentType; // 组件，立即加载
+    loadChildren?: LoadableComponent<ComponentType>; // 懒加载 webpack-chunk
+    children?: ModuleRoute[];
 }
-export const Routes:ModuleRoute[] = [
+export const Routes: ModuleRoute[] = [
     {
         path: '/pageA',
         // 大的 Page 或者说一个Module,可以用 loadable 做代码分割， 这部分路由下的代码按需加载
-        component: loadable(() => import('./Pages/pageA')),
+        loadChildren: loadable(() => import('./Pages/pageA')),
     },
     {
         path: '/pageB',
-        component: loadable(() => import('./Pages/pageB')),
+        loadChildren: loadable(() => import('./Pages/pageB')),
     },
 ];
